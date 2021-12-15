2af5a0e6a75d93cfdf735f923637352563f2782d
Thomas Palacio <palaciothomas007@gmail.com>
2021-12-15 20:57:22 -0300

    correción de desafio 4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
         <div className="App">
            <NavBar/>
-           <ItemsListContainer />
+           <ItemsListContainer greeting={ "BIENVENIDO A LISBOA" }/>
         </div>       
    );
 }
--- src/components/ItemsListContainer.js
+++ src/components/ItemsListContainer.js
@@ -4,14 +4,7 @@ import React from 'react'
 const ItemsListContainer = (props) => {
     return (
         <div class="card">
-            <div class="card-header">
-                White Slim Fit
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">$2000</h5>
-                <p class="card-text">Remera de tela hecha con hilo peruano, fabricado desde el Machupichu extraido finamente con la delicadeza necesaria para la frescura que necesitas!</p>
-                <a href="#" class="btn btn-primary">Buy</a>
-            </div>
+            <h1 className='titleinicio'>{props.greeting}</h1>
         </div>
     )
 }
